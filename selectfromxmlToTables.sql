cccaae1109ebb829f161d2ae20728768dc42ec11
Lundgrenrj <Lundgrenr18@gmail.com>
2025-03-26 22:08:56 -0600

    using select * with fix

--- selectfromxmlToTables.sql
+++ selectfromxmlToTables.sql
@@ -3,10 +3,12 @@ DECLARE @TableName2 NVARCHAR(128) = '-places-2'; -- Second table
 DECLARE @sql NVARCHAR(MAX) = '';
 DECLARE @columns NVARCHAR(MAX) = '';
 
--- Step 1: Get column names dynamically for -places-
-SELECT @columns = STRING_AGG('T.c.value(''(' + COLUMN_NAME + ')[1]'', ''NVARCHAR(MAX)'') AS [' + COLUMN_NAME + ']', ', ')
-FROM sys.columns
-WHERE OBJECT_ID = OBJECT_ID('AVE-places-.dbo.' + @TableName1);  -- Adjust schema if needed
+-- Step 1: Get column names dynamically for -places- using FOR XML PATH('')
+SELECT @columns = STUFF((
+    SELECT ', T.c.value(''(' + COLUMN_NAME + ')[1]'', ''NVARCHAR(MAX)'') AS [' + COLUMN_NAME + ']'
+    FROM sys.columns
+    WHERE OBJECT_ID = OBJECT_ID('AVE-places-.dbo.' + @TableName1) -- Adjust schema if needed
+    FOR XML PATH(''), TYPE).value('.', 'NVARCHAR(MAX)'), 1, 2, '');
 
 -- Step 2: Construct the query for -places-
 SET @sql = 'SELECT ' + @columns + ' INTO #-places- FROM @xml.nodes(''/-places-s/-places-Data/-places-'') AS T(c);';
@@ -19,9 +21,11 @@ SET @sql = '';
 SET @columns = '';
 
 -- Step 3: Get column names dynamically for -places-2
-SELECT @columns = STRING_AGG('T.c.value(''(' + COLUMN_NAME + ')[1]'', ''NVARCHAR(MAX)'') AS [' + COLUMN_NAME + ']', ', ')
-FROM sys.columns
-WHERE OBJECT_ID = OBJECT_ID('AVE-places-2.dbo.' + @TableName2);  -- Adjust schema if needed
+SELECT @columns = STUFF((
+    SELECT ', T.c.value(''(' + COLUMN_NAME + ')[1]'', ''NVARCHAR(MAX)'') AS [' + COLUMN_NAME + ']'
+    FROM sys.columns
+    WHERE OBJECT_ID = OBJECT_ID('AVE-places-2.dbo.' + @TableName2) -- Adjust schema if needed
+    FOR XML PATH(''), TYPE).value('.', 'NVARCHAR(MAX)'), 1, 2, '');
 
 -- Step 4: Construct the query for -places-2
 SET @sql = 'SELECT ' + @columns + ' INTO #-places-2 FROM @xml.nodes(''/-places-s/-places-Data/-places-2'') AS T(c);';
@@ -29,6 +33,6 @@ SET @sql = 'SELECT ' + @columns + ' INTO #-places-2 FROM @xml.nodes(''/-places-s
 -- Execute the dynamic SQL for -places-2
 EXEC sp_executesql @sql, N'@xml XML', @xml;
 
--- Verify results
-SELECT * FROM #-places-;
-SELECT * FROM #-places-2;
+-- Step 5: Query the dynamically created tables using more dynamic SQL
+SET @sql = 'SELECT * FROM #-places-; SELECT * FROM #-places-2;';
+EXEC sp_executesql @sql;
